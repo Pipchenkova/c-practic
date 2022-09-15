353b8372a9d47a09e7e5802497581118b3a15670
Olga <pipchenkova@mail.ru>
2022-09-15 22:15:31 +0300

    example2_helloUser

---
+++ example2_ HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите имя пользователя");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, ");
+Console.WriteLine(username);
+
+
